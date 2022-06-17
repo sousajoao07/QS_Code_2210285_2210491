dcf1c37413bc46b3d0f4b4676b14881d412de668
joao sousa <sousajoao07@hotmail.com>
2022-06-17 17:14:47 +0100

    added webpack.mix.js .disableNotifications()

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,9 @@ const mix = require('laravel-mix');
 //     .postCss('resources/assets/css/styles.css', 'public/css')
 //     .sass('resources/sass/app.scss', 'public/css');
 
-mix.setPublicPath('dist')
+mix
+    .disableNotifications()
+    .setPublicPath('dist')
     .js('resources/js/app.js', 'public/js')
     .js('resources/js/bootstrap', 'public/js')
     .js('resources/assets/js/scripts.js', 'public/js')
