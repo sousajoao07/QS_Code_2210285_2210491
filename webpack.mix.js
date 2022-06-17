7e63bf73ee7fded87ccbf5385490bb89567d38f6
joao sousa <sousajoao07@hotmail.com>
2022-06-17 15:51:06 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,6 @@ const mix = require('laravel-mix');
 //     .js('resources/assets/js/scripts.js', 'public/js')
 //     .postCss('resources/assets/css/styles.css', 'public/css')
 //     .sass('resources/sass/app.scss', 'public/css');
-
 mix.setPublicPath('dist')
     .js('resources/js/app.js', 'public/js')
     .js('resources/js/bootstrap', 'public/js')
