02eb2c1ebdb7af1b0ef604d82f80bb7950154798
joao sousa <sousajoao07@hotmail.com>
2022-06-17 15:14:09 +0100

    webpack.mix.js update

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,20 @@ const mix = require('laravel-mix');
  */
 
 
- mix.setPublicPath('dist')
- .js('resources/js/app.js', 'public/js')
- .js('resources/js/bootstrap', 'public/js')
- .js('resources/assets/js/scripts.js', 'public/js')
- .postCss('resources/assets/css/styles.css', 'public/css')
- .sass('resources/sass/app.scss', 'public/css')
- .copy('resources/views/index.blade.php', 'dist/public')
- .copy('resources/views/login.blade.php', 'dist/public')
- .copy('routes/api.php', 'dist/public')
- .copy('public/images/', 'dist/public/images')
+// mix
+//     .js('resources/js/app.js', 'public/js')
+//     //.js('resources/js/bootstrap.js', 'public/js')
+//     .js('resources/assets/js/scripts.js', 'public/js')
+//     .postCss('resources/assets/css/styles.css', 'public/css')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.setPublicPath('dist')
+    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/bootstrap', 'public/js')
+    .js('resources/assets/js/scripts.js', 'public/js')
+    .postCss('resources/assets/css/styles.css', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('resources/views/index.blade.php', 'dist/public')
+    .copy('resources/views/login.blade.php', 'dist/public')
+    .copy('routes/api.php', 'dist/public')
+    .copy('public/images/', 'dist/public/images')
