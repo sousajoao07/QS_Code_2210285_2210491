ae1ba537458103da00e88bc93217b1632fdd6d73
joao sousa <sousajoao07@hotmail.com>
2022-06-18 17:04:00 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,14 +20,13 @@ const mix = require('laravel-mix');
 //     .sass('resources/sass/app.scss', 'public/css');
 
 mix
-    .disableNotifications()
     .setPublicPath('dist')
     .js('resources/js/app.js', 'public/js')
     .js('resources/js/bootstrap', 'public/js')
     .js('resources/assets/js/scripts.js', 'public/js')
     .postCss('resources/assets/css/styles.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css')
-    .copy('resources/views/index.blade.php', 'dist/public')
-    .copy('resources/views/login.blade.php', 'dist/public')
-    .copy('routes/api.php', 'dist/public')
-    .copy('public/images/', 'dist/public/images')
+    // .copy('resources/views/index.blade.php', 'dist/public')
+    // .copy('resources/views/login.blade.php', 'dist/public')
+    // .copy('routes/api.php', 'dist/public')
+    // .copy('public/images/', 'dist/public/images')
