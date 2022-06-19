70070fb2e1600788fafede080d8031faa9fe7194
joao sousa <sousajoao07@hotmail.com>
2022-06-19 20:28:20 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,7 +22,3 @@ mix
     .js('resources/assets/js/scripts.js', 'public/js')
     .postCss('resources/assets/css/styles.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css')
-    // .copy('resources/views/index.blade.php', 'dist/public')
-    // .copy('resources/views/login.blade.php', 'dist/public')
-    // .copy('routes/api.php', 'dist/public')
-    // .copy('public/images/', 'dist/public/images')
